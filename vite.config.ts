9f5f5bb408eca2072101e3e0787a81e4ada2c522
David Rios <david@davidrios.co>
2026-06-06 20:40:39 -0500

    🔧 Config: Configurado para Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
+import { defineConfig } from 'vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import { devtools } from '@tanstack/devtools-vite';
+import netlify from '@netlify/vite-plugin-tanstack-start';
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import viteReact from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
-})
+  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact(), netlify()],
+});
 
-export default config
+export default config;
