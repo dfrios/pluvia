f7176dfcb5c90230fdc2b9dfab571cc40171d2e1
David Rios <david@davidrios.co>
2026-06-14 18:34:12 -0500

    ✅ Test: Prueba de acceso a Supabase

---
+++ src/routes/supa.tsx
@@ -0,0 +1,27 @@
+import { createFileRoute } from '@tanstack/react-router';
+import { supabase } from '#/utils/supabase';
+
+export const Route = createFileRoute('/supa')({
+  loader: async () => {
+    const { data: users } = await supabase.from('users').select();
+    return { users };
+  },
+  component: Supa,
+});
+
+function Supa() {
+  const { users } = Route.useLoaderData();
+
+  console.debug('>>> users', users);
+
+  return (
+    <>
+      **{users?.length}**
+      <ul>
+        {users?.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
