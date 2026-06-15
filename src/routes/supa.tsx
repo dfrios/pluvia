4b41f555c0378f414efd9bd11f8684b468ba0787
David Rios <david@davidrios.co>
2026-06-14 23:54:25 -0500

    .

--- src/routes/supa.tsx
+++ src/routes/supa.tsx
@@ -1,9 +1,29 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
+import { supabase } from '#/utils/supabase';
 
 export const Route = createFileRoute('/supa')({
-  component: RouteComponent,
-})
+  // loader: async () => {
+  //   const { data: users } = await supabase.from('users').select();
+  //   return { users };
+  // },
+  component: Supa,
+});
 
-function RouteComponent() {
-  return <div>Hello "/supa"!</div>
+function Supa() {
+  // const { users } = Route.useLoaderData();
+
+  // console.debug('>>> users', users);
+
+  // return (
+  //   <>
+  //     **{users?.length}**
+  //     <ul>
+  //       {users?.map((user) => (
+  //         <li key={user.id}>{user.name}</li>
+  //       ))}
+  //     </ul>
+  //   </>
+  // );
+
+  return <>HELLO</>;
 }
