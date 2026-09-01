a7c52283819747b6629f011f168905e20c3957a3
David Rios <david@davidrios.co>
2026-09-01 17:20:28 -0500

    🐛 Fix: Netlify no estaba totalmente deshabilitado

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import { devtools } from '@tanstack/devtools-vite';
-import netlify from '@netlify/vite-plugin-tanstack-start';
 
 import viteReact from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact(), netlify()],
+  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
 });
 
 export default config;
